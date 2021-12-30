e19a940f7d7ebfa57e4e08a110afea8b6b998735
Achim Fritz <af@achimfritz.de>
2021-12-30 18:00:40 +0100

    [WIP] sorting fix command

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -4,6 +4,9 @@ return [
     'container:integrity' => [
         'class' => \B13\Container\Command\IntegrityCommand::class,
     ],
+    'container:sorting' => [
+        'class' => \B13\Container\Command\SortingCommand::class,
+    ],
     'integrity:run' => [
         'class' => \B13\Container\Command\IntegrityCommand::class,
     ]
